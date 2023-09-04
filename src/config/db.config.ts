768b083c6f5c07413f6fd97383ec06df435b14af
TijanAyo <ayomidetijani6@gmail.com>
2023-09-04 11:38:15 +0100

    Remove unnecessary comment

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -9,7 +9,6 @@ const DB_USERNAME: string = process.env.DB_USERNAME || '';
 const DB_PASSWORD: string = process.env.DB_PASSWORD || '';
 const DB_HOST: string = process.env.DB_HOST || '';
 
-// TODO: Change to production database
 export const sequelize: Sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
